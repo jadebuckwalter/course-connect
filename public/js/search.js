247417a776595f019edcb417a1f3eb4fffad744e
Jade Buckwalter <jade.buckwalter@gmail.com>
2021-07-07 13:55:08 -0400

    Implement "My Courses"
    Add event listeners to buttons in order to add courses to the "My
    Courses" list. Revise the "clear" function so that it takes parameters
    and can be used multiple times.

--- public/js/search.js
+++ public/js/search.js
@@ -1,18 +1,20 @@
 // Search functionality
 
+let myCourses = [];
+
 // Sends a request to the backend to search for the value in the search bar
 // Returns an array containing the courses in the search results
 function searchCourses() {
     let search = document.getElementById("search").value;
     let results = "No results found.";
 
-    if (search == null) {
+    if (search === undefined) {
         document.getElementById("result").innerHTML = "Please try again.";
     }
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
             results = xhttp.responseText;
         }
     }
@@ -37,22 +39,49 @@ function formatCourseList(list) {
 
 // Display the results of the search query as buttons
 function displayResults() {
-    clearResults();
+    clear("result", "results-container");
     let courses = searchCourses();
     for (let i = 0; i < courses.length; i++) {
         let button = document.createElement("button");
         let br = document.createElement("br");
         button.id = "button" + i;
         button.innerHTML = courses[i];
+        button.addEventListener("click", function() {
+            addCourse(button.innerHTML);
+        });
         document.getElementById("result").append(button, br);
     }
 }
 
-// Clear the results of the previous search
-function clearResults() {
-    let result = document.getElementById("result");
+// Clear the contents of a div
+// id (string): id of the div
+// container (string): id of the container surrounding the div
+function clear(id, container) {
+    let result = document.getElementById(id);
     result.parentNode.removeChild(result);
     result = document.createElement("div");
-    result.id = "result";
-    document.body.appendChild(result);
+    result.id = id;
+    document.getElementById(container).appendChild(result);
+}
+
+// Add a course to the "My Courses" list, and return false if that course is already in the list
+function addCourse(name) {
+    let repeat = false;
+    myCourses.forEach(course => {
+        if (name === course) {
+            repeat = true;
+        }
+    });
+    if (!repeat) {
+        myCourses.push(name);
+        let course = document.createElement("p");
+        course.innerHTML = name;
+        document.getElementById("my-courses").append(course);
+    }
+}
+
+// Call the clear function on the "My Courses" list, and reset the array
+function clearCourses() {
+    clear('my-courses', 'courses-container');
+    myCourses = [];
 }
