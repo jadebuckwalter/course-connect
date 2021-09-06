c5c023bf433f082344f2f031a79ef7d456ae587b
Jade Buckwalter <jade.buckwalter@gmail.com>
2021-09-06 18:06:41 -0400

    Update file path for test data

--- setup/fill.sql
+++ setup/fill.sql
@@ -1,10 +1,10 @@
 -- Fill the courseconnect database with the test data
 
-LOAD DATA LOCAL INFILE "/course-connect/setup/test-data/courses.tsv" INTO TABLE courses 
+LOAD DATA LOCAL INFILE "setup/test-data/courses.tsv" INTO TABLE courses 
 FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\r\n";
 
-LOAD DATA LOCAL INFILE "/course-connect/setup/test-data/mentors.tsv" INTO TABLE mentors 
+LOAD DATA LOCAL INFILE "setup/test-data/mentors.tsv" INTO TABLE mentors 
 FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\r\n";
 
-LOAD DATA LOCAL INFILE "/course-connect/setup/test-data/connect.tsv" INTO TABLE connect 
+LOAD DATA LOCAL INFILE "setup/test-data/connect.tsv" INTO TABLE connect 
 FIELDS TERMINATED BY "\t" LINES TERMINATED BY "\r\n";
